98efd1a9df19f95462faf160375248946c86034d
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-07 15:01:39 +0100

    test api

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-console.log("JS OK");
+// console.log("JS OK");
+
+fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
+  .then((res) => res.json())
+  .then((photo) => console.log(photo));
