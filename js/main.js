dd564fb527e0e55b8fa5cfdd61c93a9cb02a18c3
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-07 15:16:51 +0100

    stampa 6 foto

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 // console.log("JS OK");
 
+const photosRow = document.getElementById("photos-row");
+console.log(photosRow);
+
 fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
   .then((res) => res.json())
-  .then((photo) => console.log(photo));
+  .then((photos) => {
+    console.log(photos);
+
+    photos.forEach((photo) => {
+      photosRow.innerHTML += `
+       <div class="col">
+            <div class="card position-relative">
+              <img
+                src="./img/pin.svg"
+                alt="Pin"
+                class="pin-svg position-absolute top-0 start-50 translate-middle"
+              />
+              <img
+                src="./img/6409f98830205.jpg"
+                class="card-img-top p-3"
+                alt="Foto 1"
+              />
+              <div class="card-body pt-0">
+                <p class="card-text text-start">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </p>
+              </div>
+            </div>
+          </div> `;
+    });
+  });
