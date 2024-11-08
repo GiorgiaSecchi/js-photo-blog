04c782c7b70a687eb7c73afcbaeb02db617b4584
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-08 15:46:26 +0100

    mod img event click  + img overlay

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 // console.log("JS OK");
 
-const photosRow = document.getElementById("photos-row");
-console.log(photosRow);
+const photosRowEl = document.getElementById("photos-row");
+console.log(photosRowEl);
 
 const overlayEl = document.getElementById("overlay");
 console.log(overlayEl);
 
-const buttonClose = document.getElementById("btn-close");
-console.log(buttonClose);
+const buttonCloseEl = document.getElementById("btn-close");
+console.log(buttonCloseEl);
+
+const imageOverlayEl = document.getElementById("imageOverlay");
+console.log(imageOverlayEl);
 
 //# CHIAMATA API
 fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
@@ -17,15 +20,16 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
 
     //* Genero le 6 foto con i dati della chiamata
     photos.forEach((photo) => {
-      photosRow.innerHTML += `
+      photosRowEl.innerHTML += `
        <div class="col px-4">
-            <div id="${photo.id}" class="card position-relative h-100 shadow">
+            <div class="card position-relative h-100 shadow">
               <img
                 src="./img/pin.svg"
                 alt="Pin"
                 class="pin-svg position-absolute top-0 start-50 translate-middle"
               />
               <img
+                data-id="${photo.id}"
                 src="${photo.url}"
                 class="card-img-top p-3"
                 alt="Photo: ${photo.id}"
@@ -40,17 +44,21 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
     });
 
     //* Al click immagine rimozione d-none (compare ovelay)
-    const photoImageEL = document.querySelectorAll(".card-img-top");
-    console.log(photoImageEL);
+    const imagesCardEl = document.querySelectorAll(".card-img-top");
+    console.log(imagesCardEl);
 
-    photoImageEL.forEach((image) => {
-      image.addEventListener("click", () =>
-        overlayEl.classList.remove("d-none")
-      );
+    imagesCardEl.forEach((image) => {
+      image.addEventListener("click", () => {
+        const photoId = image.getAttribute("data-id");
+        console.log(`ID Immagine: ${photoId}`);
+
+        imageOverlayEl.src = image.src;
+        overlayEl.classList.remove("d-none");
+      });
     });
 
     //* Bottone che riaggiunge d-none (scompare overlay)
-    buttonClose.addEventListener("click", () =>
+    buttonCloseEl.addEventListener("click", () =>
       overlayEl.classList.add("d-none")
     );
   });
